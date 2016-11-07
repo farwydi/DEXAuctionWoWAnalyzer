85e07ea9aeead9a74ba19e3b77b4b37381e421e2
Zharikov Leonid <Zharikov Leonid>
2016-11-07 18:22:21 +0300

    first init npm gulp

---
+++ htdocs/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>
+		body {
+
+		}
+		.c {
+			width: 280px;
+			margin: 0 auto;
+			background-color: #ff0;
+		}
+		.b {
+			height: 100%;
+			background-color: #0ff;
+			width: 50px;
+			display: inline-block;
+			text-align: center;
+			padding: 0 1px;
+		}
+		.b1 {
+			height: 100%;
+			background-color: #0ff;
+			width: 50px;
+			display: inline-block;
+			text-align: center;
+			padding-right: 1px;
+		}
+		.b2 {
+			height: 100%;
+			background-color: #0ff;
+			width: 50px;
+			display: inline-block;
+			text-align: center;
+			padding-left: 1px;
+		}
+	</style>
+</head>
+<body>
+	<div class="c">asd
+	</div>
+</body>
+</html>
